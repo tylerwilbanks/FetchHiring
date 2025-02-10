5b6cb67eaf7ea8e5a4530c7c63e8fc966e0f356f
tylerwilbanks <tyler.wilbanks45@gmail.com>
2025-02-10 00:01:38 -0600

    create project scaffold, create remote module.

---
+++ app/src/main/java/com/minutesock/fetchhiring/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.minutesock.fetchhiring
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.minutesock.fetchhiring.ui.theme.FetchHiringTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FetchHiringTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.fetch_rewards_logo),
+            contentDescription = "Fetch rewards logo"
+        )
+    }
+}
